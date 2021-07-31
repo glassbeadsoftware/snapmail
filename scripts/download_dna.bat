920424c2ae9f99c3e3b02d603397fbf550c5daba
damien <doute.d@gmail.com>
2021-07-30 22:25:16 -0400

    Implemented download dna scripts

--- scripts/download_dna.bat
+++ scripts/download_dna.bat
@@ -1,18 +1,16 @@
 @echo off
 
-curl -s https://api.github.com/repos/glassbeadsoftware/snapmail-rsm/releases/latest | grep "browser_download_url.*dna" | cut -d '"' -f 4
+REM curl -s https://api.github.com/repos/glassbeadsoftware/snapmail-rsm/releases/latest | grep "browser_download_url.*dna" | cut -d '"' -f 4
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`"curl -s https://api.github.com/repos/glassbeadsoftware/snapmail-rsm/releases/latest | grep 'browser_download_url.*dna'"`) DO (
 SET version=%%F
 )
-echo %version%
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`cut %version% -d '"' -f 4`) DO (
-SET link=%%F
+REM echo %version%
+for /f "tokens=1,2,3 delims= " %%a in ("%version%") do (
+  set quoted=%%b
 )
+REM echo %quoted%
+set link=%quoted:~1,-1%
 echo %link%
 
-REM FOR /F "tokens=* USEBACKQ" %%F IN (`cat link.txt`) DO (
-REM SET link=%%F
-REM )
-REM echo %link%
+bitsadmin /transfer myDownloadJob /download /priority high %link% %cd%\dna\snapmail.dna
