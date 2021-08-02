215c62f54c6ff09d15ef955cd52ebbd7d4e4c4e5
damien <doute.d@gmail.com>
2021-08-02 14:23:35 -0400

    missing argument: platform

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,12 @@ echo \* Create new Snapmail happ Release
 echo !!! Make sure DNA has been updated to latest first !!!
 echo
 
+# Check pre-conditions
+if [ $# != 1 ]; then
+  echo 1>&2 "$0: Aborting. Missing argument: target platform"
+  exit 2
+fi
+
 start=`date +%s`
 
 # Starting Release process
