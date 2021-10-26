2f8d6fd1829e7713631d9e1a943988a903fbb1a8
damien <doute.d@gmail.com>
2021-10-26 16:31:42 -0400

    workflow fix

--- scripts/workflow/dna-pack.sh
+++ scripts/workflow/dna-pack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Compile the WASM
-cargo build --release --target wasm32-unknown-unknown --manifest-path build/snapmail-rsm/zomes/Cargo.toml
+cargo build --release --target wasm32-unknown-unknown --manifest-path build/snapmail-rsm/zomes/snapmail/Cargo.toml
 # Pack DNAs
 hc dna pack --output=dna/snapmail.dna build/snapmail-rsm/snapmail.dna.workdir
