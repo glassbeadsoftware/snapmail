e55aa9f65ae33336e49534cd9c3550f79a87055d
damien <doute.d@gmail.com>
2022-04-26 19:48:33 -0400

    added build-webapp step to workflow

--- scripts/npm/dna-pack.sh
+++ scripts/npm/dna-pack.sh
@@ -3,3 +3,4 @@
 cargo build --release --target wasm32-unknown-unknown --manifest-path build/snapmail-rsm/zomes/snapmail/Cargo.toml
 # Pack DNAs
 hc dna pack --output=dna/snapmail.dna build/snapmail-rsm/snapmail.dna.workdir
+hc app pack --output=dna/snapmail.happ build/snapmail-rsm/snapmail.dna.workdir
