947b5dcba4da69d965f7fb67a3f379d89e1894e0
ddd <doute.d@gmail.com>
2023-02-05 16:42:21 -0500

    copy runner binary fix attempt

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -6,13 +6,16 @@ binFolder="electron/bin"
 moduleFolder="node_modules/@lightningrodlabs/electron-holochain/binaries"
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  cp $moduleFolder/holochain-runner $binFolder/holochain-runner
+  bin = holochain-runner
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
-  cp $moduleFolder/holochain-runner $binFolder/holochain-runner
+  bin = holochain-runner
 elif [[ $OSTYPE == "cygwin" ]]; then
   # POSIX compatibility layer and Linux environment emulation for Windows
-  cp $moduleFolder/holochain-runner.exe $binFolder/holochain-runner.exe
+  bin = holochain-runner.exe
 elif [[ $OSTYPE == "msys" ]]; then
   # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-  cp $moduleFolder/holochain-runner.exe $binFolder/holochain-runner.exe
+  bin = holochain-runner.exe
 fi
+
+ls $moduleFolder
+cp $moduleFolder/$bin $binFolder/$bin
