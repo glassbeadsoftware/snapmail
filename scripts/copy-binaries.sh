dbc95795a3308b1e3cd31d25685b59b84f771958
ddd <doute.d@gmail.com>
2023-02-05 17:17:10 -0500

    2nd attempt

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo $OSTYPE
+echo copy-binaries.sh for $OSTYPE
 
 binFolder="electron/bin"
 moduleFolder="node_modules/@lightningrodlabs/electron-holochain/binaries"
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  bin = holochain-runner
+  bin="holochain-runner"
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
-  bin = holochain-runner
+  bin="holochain-runner"
 elif [[ $OSTYPE == "cygwin" ]]; then
   # POSIX compatibility layer and Linux environment emulation for Windows
-  bin = holochain-runner.exe
+  bin="holochain-runner.exe"
 elif [[ $OSTYPE == "msys" ]]; then
   # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-  bin = holochain-runner.exe
+  bin="holochain-runner.exe"
 fi
 
-ls $moduleFolder
+
 cp $moduleFolder/$bin $binFolder/$bin
