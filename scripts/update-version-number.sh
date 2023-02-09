c59a1df5a4c48a4017520e6de040ca8b3340bbfd
damien <doute.d@gmail.com>
2023-02-09 15:49:41 -0500

    slimer workflow

--- scripts/update-version-number.sh
+++ scripts/update-version-number.sh
@@ -29,16 +29,6 @@ OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./webapp/package.json | sed 's/"//g
 echo "./webapp/package.json $OLD_VER -> $1"
 sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./webapp/package.json
 
-# Change .github/workflows/release.yml
-OLD_VER=`awk -F ": " '/SNAPMAIL_VERSION/{print $2}' ./.github/workflows/release.yml`
-echo "./.github/workflows/release.yml '$OLD_VER' -> '$1'"
-sed -i "s/$OLD_VER/$1/" ./.github/workflows/release.yml
-
-# Change .github/workflows/test.yml
-OLD_VER=`awk -F ": " '/SNAPMAIL_VERSION/ {print $2}' ./.github/workflows/test.yml`
-echo "./.github/workflows/test.yml '$OLD_VER' -> '$1'"
-sed -i "s/$OLD_VER/$1/" ./.github/workflows/test.yml
-
 
 # Change electron/web/splashscreen.html
 LINE=`echo version $1`
